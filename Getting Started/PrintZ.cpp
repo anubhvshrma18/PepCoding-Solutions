77681a1c9c24e8d63b3ebaec9afa50b287fa4b0b
Anubhav Sharma <anubhvshrma18@gmail.com>
2021-01-30 01:09:56 +0530

    third question

---
+++ Getting Started/PrintZ.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char** agrc){
+    //write your code here
+    cout <<"*****" << endl;
+    cout <<"   *" << endl;
+    cout <<"  *" << endl;
+    cout <<" *" << endl;
+    cout <<"*****" << endl;
+    return 0;
+
+}
