bfc3199c96b3094fb5614304291c72db71b0d1fa
Anubhav Sharma <anubhvshrma18@gmail.com>
2021-02-05 00:15:35 +0530

    Updated recursion

---
+++ Introduction-to-Recursion/Print-ZigZag.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n) {
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+    }
+
+}
