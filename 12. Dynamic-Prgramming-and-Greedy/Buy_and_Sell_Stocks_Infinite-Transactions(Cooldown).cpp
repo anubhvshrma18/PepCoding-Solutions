290d7438f28ccb246be6283d506254310e58c794
Anubhav Sharma <anubhvshrma18@gmail.com>
2021-05-25 06:02:29 +0530

    Update Buy_and_Sell_Stocks_Infinite-Transactions(Cooldown).cpp

--- 12. Dynamic-Prgramming-and-Greedy/Buy_and_Sell_Stocks_Infinite-Transactions(Cooldown).cpp
+++ 12. Dynamic-Prgramming-and-Greedy/Buy_and_Sell_Stocks_Infinite-Transactions(Cooldown).cpp
@@ -2,7 +2,42 @@
 using namespace std;
 
 int infiTranswithCooldown(int arr[],int n){
+	int obsp=-arr[0],ossp=0,ocsp=0;
+	for(int i=-1;i<n;i++){
+		int nbsp=0,nssp=0,ncsp=0;
+		//buy
+		if(ocsp-arr[i]>obsp) {
+			nbsp=ocsp-arr[i];
+		}
+		else{
+			nbsp=obsp;
+		}
 
+
+		// sell
+		if(obsp+arr[i]>ossp) {
+			nssp=obsp+arr[i];
+		}
+		else{
+			nssp=ossp;
+		}
+
+
+		// cooldown
+		if(ossp>ocsp) {
+			ncsp=ossp;
+		}
+		else{
+			ncsp=ocsp;
+		}
+
+		obsp=nbsp;
+		ossp=nssp;
+		ocsp=ncsp;
+
+	}
+
+	return ossp;
 }
 
 
