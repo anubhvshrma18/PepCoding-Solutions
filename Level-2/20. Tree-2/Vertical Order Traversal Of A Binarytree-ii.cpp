1d5ebd43e768ad923bf007c8dd584e217df1f017
Anubhav Sharma <anubhvshrma18@gmail.com>
2021-11-23 02:38:56 +0530

    Tree - 2

---
+++ Level-2/20. Tree-2/Vertical Order Traversal Of A Binarytree-ii.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val = 0;
+    TreeNode *left = nullptr;
+    TreeNode *right = nullptr;
+
+    TreeNode(int val)
+    {
+        this->val = val;
+    }
+};
+
+vector<vector<int>> verticalOrderTraversal(TreeNode *root){
+    map<int, map<int, multiset<int>>> store;
+        queue<pair<TreeNode*, pair<int, int>>> todo;
+        todo.push({root, {0, 0}});
+        while (!todo.empty()) {
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* node = p.first;
+            int x = p.second.first, y = p.second.second;
+            store[x][y].insert(node -> val);
+            if (node -> left) {
+                todo.push({node -> left, {x - 1, y + 1}});
+            }
+            if (node -> right) {
+                todo.push({node -> right, {x + 1, y + 1}});
+            }
+        }
+    
+        vector<vector<int>> ans;
+        for(auto m:store){
+            vector<int> v;
+            for(auto a:m.second){
+                v.insert(v.end(),a.second.begin(),a.second.end());
+
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    
+}
+
+// input_section=================================================
+
+TreeNode *createTree(vector<int> &arr, vector<int> &IDX)
+{
+
+    if (IDX[0] > arr.size() || arr[IDX[0]] == -1)
+    {
+        IDX[0]++;
+        return nullptr;
+    }
+
+    TreeNode *node = new TreeNode(arr[IDX[0]++]);
+    node->left = createTree(arr, IDX);
+    node->right = createTree(arr, IDX);
+
+    return node;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> IDX(1, 0);
+    TreeNode *root = createTree(arr, IDX);
+
+    vector<vector<int>> ans = verticalOrderTraversal(root);
+    int idx = 0;
+    for (vector<int> &i : ans)
+    {
+        cout << idx++ << " -> ";
+        for (int j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
