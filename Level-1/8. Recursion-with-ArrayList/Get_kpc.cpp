44276087bbb6404f08b1a671cadfb594d0599a10
Subham Deep <84119874+subham-202@users.noreply.github.com>
2023-02-25 14:08:38 +0530

    Update Get_kpc.cpp

--- Level-1/8. Recursion-with-ArrayList/Get_kpc.cpp
+++ Level-1/8. Recursion-with-ArrayList/Get_kpc.cpp
@@ -1,60 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-string getString(char n){
-    if(n=='0'){
-        return ".;";
+
+string codes[] = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+string getString(char n) {
+    int idx = n - '0';
+    if (idx < 0 || idx > 9) {
+        return "";
     }
-    else if(n=='1'){
-        return "abc";
-    }
-    else if(n=='2'){
-        return "def";
-    }
-    else if(n=='3'){
-        return "ghi";
-    }
-    else if(n=='4'){
-        return "jkl";
-    }
-    else if(n=='5'){
-        return "mno";
-    }
-    else if(n=='6'){
-        return "pqrs";
-    }
-    else if(n=='7'){
-        return "tu";
-    }
-    else if(n=='8'){
-        return "vwx";
-    }
-    else{
-        return "yz";
-    }
+    return codes[idx];
 }
 
 vector<string> getkpc(string s){
-    if(s.length()==0){
+    if(s.length() == 0){
         vector<string> v;                             
         string output="";
         v.push_back(output);
         return v;
     }
-    string output=getString(s[0]);
-    vector<string> rec=getkpc(s.substr(1));
+    string output = getString(s[0]);
+    vector<string> rec = getkpc(s.substr(1));
     vector<string> result;
-    for(int i=0;i<output.length();i++){
-        for(int j=0;j<rec.size();j++){
-            result.push_back(output[i]+rec[j]);
+    for(int i=0; i<output.length(); i++){
+        for(int j=0; j<rec.size(); j++){
+            string code = output.substr(i, 1);
+            result.push_back(code + rec[j]);
         }  
     }
     return result;
-
 }
+
 int main() {
     string s;
     cin >> s;
-    vector<string> res=getkpc(s);
+    vector<string> res = getkpc(s);
     int cnt = 0;
     cout << '[';
     for (string str : res){
@@ -65,4 +44,5 @@ int main() {
         cnt++;
     }
     cout << ']';
+    return 0;
 }
